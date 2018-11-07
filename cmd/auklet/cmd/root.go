7f643eabffb1ca4bb853b70accfc4c3b325ba07b
Bastiaan Schaap <bastiaan.schaap@siteminds.nl>
2018-11-07 22:18:38 +0100

    Add flags and config

--- cmd/auklet/cmd/root.go
+++ cmd/auklet/cmd/root.go
@@ -4,16 +4,50 @@ import (
 	"fmt"
 	"os"
 
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-// RootCmd represents the base command when called without any subcommands
-var RootCmd = &cobra.Command{
-	Use:   "auklet",
-	Short: "A service autoscaler for Docker Swarm",
-	Long:  `
+var (
+	// RootCmd represents the base command when called without any subcommands
+	RootCmd = &cobra.Command{
+		Use:   "auklet",
+		Version: fmt.Sprintf("%s-%s (built by %s on %s)", version, gitTag, buildUser, buildDate),
+		Short: "A service autoscaler for Docker Swarm",
+		Long:  `
 Autoscale services in Docker Swarm based on service
 labels by querying Prometheus.`,
+	}
+
+	cfgFile string
+)
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.auklet)")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".auklet" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".auklet")
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		// Ignore it, config file is optional..
+	}
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
